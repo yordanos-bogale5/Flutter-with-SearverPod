00e6f5faa871e2fcc822e9364b2640d976a8073a
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-04-29 16:50:24 +0300

    date time ytils

---
+++ lib/core/utils/date_time_utils.dart
@@ -0,0 +1,16 @@
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+const String dateTimeFormatPattern = 'dd/MM/yyyy';
+
+extension DateTimeExtension on DateTime {
+  String format({
+    Stringpattern = dateTimeFormatPattern,
+    String? locale,
+  }) {
+    if (locale != null && locale.isNotEmpty) {
+      initializeDateFormatting(locale);
+    }
+    return DateFormat(Pattern, locale).format(this);
+  }
+}
