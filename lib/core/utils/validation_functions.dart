c44d9e48c80e5e714d0b93cfbec9d7cf648f2f25
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-04-29 19:20:37 +0300

    validation function

---
+++ lib/core/utils/validation_functions.dart
@@ -0,0 +1,99 @@
+// check if the string is email
+bool isValidEmail(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+ (\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+//password should have,
+//at least upper case letter
+//at least lower case letter
+// at least a digit
+//at least a special character [@£^%£]
+// leangth of at least four
+//no white space allowed
+
+bool isValidPassword(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern =
+        r'^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$';
+
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+// checks if the string consist only Alphabet .(No white space)
+
+bool isText(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern = r'^[a-zA-Z]+$';
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+// Check if the  string consist only numeric .
+
+bool isNumeric(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern = r'^\d+$';
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+// check is string is phone number
+
+bool isValidPhone(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    if (inputString.length > 16 || inputString.length < 6) return false;
+    const pattern =
+        r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
