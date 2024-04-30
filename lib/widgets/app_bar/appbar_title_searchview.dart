a7343d9cfe8374c3b7dd01090b9be9097c1af5d9
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-05-01 02:10:57 +0300

    app bar title search view

---
+++ lib/widgets/app_bar/appbar_title_searchview.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:virtual/core/app_export.dart';
+
+// ignore: must_be_immutable
+class AppbarTitleSearchView extends StatelessWidget {
+  AppbarTitleSearchView({
+    Key? key,
+    this.hintText,
+    this.controller,
+    this.margin,
+  }) : super(
+          key: key,
+        );
+
+  String? hinttext;
+
+  TextEditingController? controller;
+
+  EdgeInsetsGeometry? margin;
+
+  Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: CustomSearchView(
+        width:263.h,
+        controller: controller,
+        hinttext : "lbl_search_product".tr,
+      ),
+    );
+  }
+}
