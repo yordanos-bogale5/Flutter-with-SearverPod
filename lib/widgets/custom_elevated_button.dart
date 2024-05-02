df5390d7213c391abb277e26ec7a13651b1f6ff9
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-05-02 08:46:50 +0300

    custom outlined button

--- lib/widgets/custom_elevated_button.dart
+++ lib/widgets/custom_elevated_button.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:virtual/core/app_export.dart';
-
 import 'package:virtual/widgets/base_button.dart';
 
-class CustomElevatedButton extends StatelessWidget {
+class CustomElevatedButton extends BaseButton {
   CustomElevatedButton({
     Key? key,
     this.decoration,
     this.leftIcon,
     this.rightIcon,
     EdgeInsets? margin,
-    VoidCallBack? onPressed,
+    VoidCallback? onPressed,
     ButtonStyle? buttonStyle,
     Alignment? alignment,
     TextStyle? buttonTextStyle,
@@ -34,9 +32,10 @@ class CustomElevatedButton extends StatelessWidget {
   final Widget? leftIcon;
   final Widget? rightIcon;
 
+
   @override
   Widget build(BuildContext context) {
-    return alignment! + null
+    return alignment != null
         ? Align(
             alignment: alignment ?? Alignment.center,
             child: buildElevatedButtonWidget,
@@ -49,14 +48,14 @@ class CustomElevatedButton extends StatelessWidget {
         width: this.width ?? double.maxFinite,
         margin: margin,
         decoration: decoration,
-        child: ElevatedButton(
+        child: OutlinedButton(
           style: buttonStyle,
           onPressed: isDisabled ?? false ? null : onPressed ?? () {},
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              leftIcon ?? const SizedBox.shirnk(),
+              leftIcon ?? const SizedBox.shrink(),
               Text(
                 text,
                 style: buttonTextStyle ??
---
+++ lib/widgets/custom_outlined_button.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:virtual/core/app_export.dart';
+import 'package:virtual/widgets/base_button.dart';
+
+class CustomOutlinedButton extends BaseButton {
+  CustomOutlinedButton({
+    Key? key,
+    this.decoration,
+    this.leftIcon,
+    this.rightIcon,
+    EdgeInsets? margin,
+    VoidCallback? onPressed,
+    ButtonStyle? buttonStyle,
+    Alignment? alignment,
+    TextStyle? buttonTextStyle,
+    bool? isDisabled,
+    double? height,
+    double? width,
+    required String text,
+  }) : super(
+          text: text,
+          onPressed: onPressed,
+          buttonStyle: buttonStyle,
+          isDisabled: isDisabled,
+          buttonTextStyle: buttonTextStyle,
+          height: height,
+          width: width,
+          alignment: alignment,
+          margin: margin,
+        );
+  final BoxDecoration? decoration;
+  final Widget? leftIcon;
+  final Widget? rightIcon;
+  final Widget? label;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: buildOutlinedButtonWidget,
+          )
+        : buildOutlinedButtonWidget;
+  }
+
+  Widget get buildOutlinedButtonWidget => Container(
+        height: this.height ?? 57.v,
+        width: this.width ?? double.maxFinite,
+        margin: margin,
+        decoration: decoration,
+        child: OutlinedButton(
+          style: buttonStyle,
+          onPressed: isDisabled ?? false ? null : onPressed ?? () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              leftIcon ?? const SizedBox.shrink(),
+              Text(
+                text,
+                style: buttonTextStyle ??
+                    CustomTextStyles.titleSmallOnPrimaryContainer_1,
+              ),
+              rightIcon ?? const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      );
+}
